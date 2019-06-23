ebe93508317ce7d8590494b53dd30bf76d19daa6
David Shoots <david@algorand.com>
2019-06-22 21:37:58 -0400

    Checkin - partial

--- docker/releases/build_and_deploy_docker.sh
+++ docker/releases/build_and_deploy_docker.sh
@@ -9,5 +9,5 @@ set -e
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd ${SCRIPTPATH}
 
-docker build -f Dockerfile-testnet . -t algorand/testnet-telem:latest
+docker build -f Dockerfile . -t algorand/stable:latest
 docker push algorand/testnet-telem:latest
